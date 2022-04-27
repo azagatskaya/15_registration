287fc7faddfc4c20bd7fcd29a4445d269eb9e928
Alya Zagatskaya <alya.zagacki@gmail.com>
2022-04-27 22:54:10 +0300

    220427_2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-// let formInputs = document.querySelectorAll(".reg-form__input");
-
-// formInputs.forEach((e) => {
-//   e.addEventListener("focusout", check);
-// });
-
 const inputsData = {
   email: {
     reg: /[@\.]/,
@@ -21,10 +15,32 @@ const inputsData = {
     label: 'Введите пароль еще раз',
     posMsg: '',
     negMsg: 'Пароли не совпадают'
+  },
+  countries: {
+    label: 'Страна',
+    posMsg: '',
+    negMsg: ''
+  },
+  firstName: {
+    reg: /[a-zA-Z][^0-9]/i,
+    label: 'Имя',
+    posMsg: '',
+    negMsg: 'Используйте только буквы, апострофы, дефисы, запятые, пробелы и точки.'
+  },
+  familyName: {
+    reg: /[a-zA-Z][^0-9]/i,
+    label: 'Фамилия',
+    posMsg: '',
+    negMsg: 'Используйте только буквы, апострофы, дефисы, запятые, пробелы и точки.'
   }
 }
 
 document.querySelector("#email").addEventListener('focusout', checkEmail);
+document.querySelector("#password1").addEventListener('focusout', checkPassword1);
+document.querySelector("#password2").addEventListener('focusout', checkPassword2);
+document.querySelector("#countries").addEventListener('focusout', checkCountries);
+document.querySelector("#firstname").addEventListener('focusout', checkFirstName);
+document.querySelector("#familyname").addEventListener('focusout', checkFamilyName);
 
 function checkEmail() {
   const {
@@ -33,49 +49,102 @@ function checkEmail() {
     posMsg,
     negMsg
   } = inputsData.email;
-  console.log(label);
-  elementLabel = "Адрес электронной почты";
-  textMsgPositive =
-    "Вам будет отправлено электронное письмо с подтверждением";
-  textMsgNegative = "Это недопустимый адрес электронной почты";
+  console.log(reg, label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag = validateValue(reg, this.value);
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
 
-  if (this.value != "") {
-    if (reg === true) {
-      setPositiveResult(this, posMsg);
+function checkPassword1() {
+  const {
+    reg,
+    label,
+    posMsg,
+    negMsg
+  } = inputsData.password1;
+  console.log(reg, label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag = (validateValue(reg, this.value) && this.value.length >= 8) ? true : false;
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
+
+function checkPassword2() {
+  const {
+    label,
+    posMsg,
+    negMsg
+  } = inputsData.password2;
+  console.log(label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag =
+    (this.value === document.querySelector('#password1').value) ? true : false;
+  console.log(regFlag);
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
+
+function checkCountries() {
+  const {
+    label,
+    posMsg,
+    negMsg
+  } = inputsData.countries;
+  console.log(label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag =
+    (this.value !== '') ? true : false;
+  console.log(regFlag);
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
+
+function checkFirstName() {
+  const {
+    reg,
+    label,
+    posMsg,
+    negMsg
+  } = inputsData.firstName;
+  console.log(label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag = validateValue(reg, this.value);
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
+
+function checkFamilyName() {
+  const {
+    reg,
+    label,
+    posMsg,
+    negMsg
+  } = inputsData.familyName;
+  console.log(label, posMsg, negMsg);
+  console.log(this.value);
+  let regFlag = validateValue(reg, this.value);
+  showMessage(this, regFlag, posMsg, negMsg, label);
+}
+
+
+
+function showMessage(element, regFlag, posMsg, negMsg, label) {
+  if (element.value != '') {
+    if (regFlag === true) {
+      setPositiveResult(element, posMsg);
     } else {
-      setNegativeResult(this, negMsg);
+      setNegativeResult(element, negMsg);
     }
   } else {
-    setNegativeResult(this, label);
+    setNegativeResult(element, label);
   }
-
-  // document.querySelector(".email-status > img").src =
-  //   icon; // присваиваем нужную иконку (pos/neg)
-  // this.style.borderBottomColor = borderColor; // меняем цвет нижней границы
-  // document.querySelector(
-  //   ".input-" + elementId + " .status-message__text"
-  // ).innerText = textMsg; // присваиваем текст сообщения
-  // document.querySelector(
-  //   ".input-" + elementId + " .status-message img"
-  // ).style.width = iconSize; // размер иконки ширина
-  // document.querySelector(
-  //   ".input-" + elementId + " .status-message img"
-  // ).style.height = iconSize; // размер иконки высота
-  // document.querySelector(
-  //   ".input-" + elementId + " .status-message"
-  // ).style.display = "flex"; // показываем сообщение с результатом
-  // document.querySelector(
-  //   ".input-" + elementId + " .status-message img"
-  // ).style.display = "block"; // показывает картинку ок/не ок
 }
 
 function setPositiveResult(element, msg) {
   removeIconClass(element, 'img--invalid');
+  addIconClass(element, 'img--valid'); // размер иконки ширина высота
   if (isClassInList(element, 'input--invalid')) {
     element.classList.remove('input--invalid');
   }
-  element.classList.add('input--valid'); // меняем цвет нижней границы
-  addIconClass(element, 'img--valid'); // размер иконки ширина высота
+  if (!isClassInList(element, 'input--valid')) {
+    element.classList.add('input--valid'); // меняем цвет нижней границы
+  }
   setPositiveIcon(element.id); // присваиваем нужную иконку (pos/neg)
   setStatusMsg(element.id, msg); // присваиваем текст сообщения
   displayImg(element.id); // показывает картинку ок/не ок
@@ -84,11 +153,13 @@ function setPositiveResult(element, msg) {
 
 function setNegativeResult(element, msg) {
   removeIconClass(element, 'img--valid');
+  addIconClass(element, 'img--invalid'); // размер иконки ширина высота
   if (isClassInList(element, 'input--valid')) {
     element.classList.remove('input--valid');
   }
-  element.classList.add('input--invalid'); // меняем цвет нижней границы
-  addIconClass(element, 'img--invalid'); // размер иконки ширина высота
+  if (!isClassInList(element, 'input--invalid')) {
+    element.classList.add('input--invalid');
+  } // меняем цвет нижней границы
   setNegativeIcon(element.id); // присваиваем нужную иконку (pos/neg)
   setStatusMsg(element.id, msg); // присваиваем текст сообщения
   displayImg(element.id); // показывает картинку ок/не ок
@@ -104,20 +175,26 @@ function setNegativeIcon(elementId) {
 }
 
 function isClassInList(element, className) {
+  res = false;
   element.classList.forEach(el => {
     if (el === className) {
-      return true;
+      res = true;
     }
   });
+  return res;
 }
 
 function addIconClass(element, className) {
-  document.querySelector(`.${element.id}-status img`).classList.add(className);
+  const icon = document.querySelector(`.${element.id}-status img`);
+  if (!isClassInList(icon, className)) {
+    icon.classList.add(className);
+  }
 }
 
 function removeIconClass(element, className) {
-  if (isClassInList(element, 'img--invalid')) {
-    document.querySelector(`.${element.id}-status img`).classList.remove(className);
+  const icon = document.querySelector(`.${element.id}-status img`);
+  if (isClassInList(icon, className)) {
+    icon.classList.remove(className);
   }
 }
 
@@ -134,48 +211,7 @@ function displayStatusMsg(elementId) {
 }
 
 function check() {
-  let textMsg;
-  let borderColor;
-  let icon;
-  let iconSize;
-  let elementId = this.id;
-  let reg;
-  let elementLabel;
-  let textMsgPositive;
-  let textMsgNegative;
   switch (elementId) {
-    case "email":
-      reg = /[@\.]/;
-      reg = validateValue(reg, this.value);
-      elementLabel = "Адрес электронной почты";
-      textMsgPositive =
-        "Вам будет отправлено электронное письмо с подтверждением";
-      textMsgNegative = "Это недопустимый адрес электронной почты";
-      break;
-    case "password1":
-      reg = /[a-zA-Z0-9]+\W+|\W+[a-zA-Z0-9]+/g;
-      if (validateValue(reg, this.value) && this.value.length >= 8) {
-        reg = true;
-      } else {
-        reg = false;
-      }
-      elementLabel = "Пароль";
-      textMsgPositive = "Пароль соответствует требованиям";
-      textMsgNegative = "Пароль не соответствует требованиям безопасности";
-      break;
-    case "password2":
-      reg =
-        this.value == document.querySelector("#password1").value ? true : false;
-      console.log(reg);
-      elementLabel = "Введите пароль еще раз";
-      textMsgPositive = "";
-      textMsgNegative = "Пароли не совпадают";
-      break;
-    case "countries":
-      elementLabel = "Страна";
-      textMsgPositive = "";
-      textMsgNegative = "";
-      break;
     case "first-name":
       reg = /[a-zA-Z][^0-9]/i;
       elementLabel = "Имя";
@@ -207,47 +243,7 @@ function check() {
       textMsgNegative = "";
       break;
     case "agreement":
-      //
-      break;
-    default:
-      //
   }
-  if (this.value != "") {
-    if (reg === true) {
-      textMsg = textMsgPositive;
-      borderColor = "#6A9D67";
-      icon = "./assets/images/checkcircle-s.svg";
-      iconSize = "20px";
-    } else {
-      textMsg = textMsgNegative;
-      borderColor = "#C74634";
-      icon = "./assets/images/error-s.svg";
-      iconSize = "16px";
-    }
-  } else {
-    textMsg = "Требуется " + elementLabel;
-    borderColor = "#C74634";
-    icon = "./assets/images/error-s.svg";
-    iconSize = "16px";
-  }
-  document.querySelector(".input-" + elementId + " .status-message img").src =
-    icon;
-  this.style.borderBottomColor = borderColor;
-  document.querySelector(
-    ".input-" + elementId + " .status-message__text"
-  ).innerText = textMsg;
-  document.querySelector(
-    ".input-" + elementId + " .status-message img"
-  ).style.width = iconSize;
-  document.querySelector(
-    ".input-" + elementId + " .status-message img"
-  ).style.height = iconSize;
-  document.querySelector(
-    ".input-" + elementId + " .status-message"
-  ).style.display = "flex";
-  document.querySelector(
-    ".input-" + elementId + " .status-message img"
-  ).style.display = "block";
 }
 
 function validateValue(reg, val) {
